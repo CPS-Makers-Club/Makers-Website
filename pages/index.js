8414b2dbe41318336bdb10c690c79b3c12b0ec12
Jim Fang <jfang.cv.ca.us@gmail.com>
2022-08-19 13:18:10 -0700

    working on first pg hero

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import { useEffect, useRef } from "react";
 import { Navbar } from '../components/navbar'
 
 export default function Home() {
+  const el = useRef(null)
+
   return (
     <div>
       <Head>
@@ -14,10 +18,33 @@ export default function Home() {
         <meta name="theme-color" content="#ffffff" />
       </Head>
 
-      <Navbar />
-      <h1>
-        Hello World!
-      </h1>
+      <div className='flex flex-col h-screen'>
+        <Navbar />
+        <div className='bg-white text-black overflow-hidden items-center flex flex-grow'>
+
+          <div className='flex flex-auto items-center justify-center'>
+
+            <Image
+              src='/rocket.svg'
+              alt='logo'
+              width={255}
+              height={256}
+              // todo: add animations
+              className='animate-fade'
+            >
+            </Image>
+
+          </div>
+
+        </div>
+
+        <div className='text-center space-y-4 font-sans'>
+          <h2 className='text-7xl animate-fade'>CPS Makers</h2>
+          {/* <span ref={el} className='text-4xl animate-fade'></span> */}
+        </div>
+
+      </div>
     </div>
+
   )
 }
